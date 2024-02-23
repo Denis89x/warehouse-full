b69416e759a198e6b50d32c01ff04a1dd2cdca7a
Lebenkov Denis <lebenkov.denis@mail.ru>
2024-02-23 23:19:40 +0300

    order composition excel service has been refactored

--- src/main/java/dev/lebenkov/warehouse/api/service/OrderCompositionExcelServiceImpl.java
+++ src/main/java/dev/lebenkov/warehouse/api/service/OrderCompositionExcelServiceImpl.java
@@ -34,7 +34,15 @@ public class OrderCompositionExcelServiceImpl implements OrderCompositionExcelSe
         this.stylizeExcelService = stylizeExcelService;
     }
 
-    private void writeDeliveryNoteExcel(XSSFWorkbook workbook, LocalDate startDate, LocalDate endDate) {
+    private XSSFSheet createSheet(XSSFWorkbook workbook, String sheetName) {
+        int index = workbook.getSheetIndex(sheetName);
+        if (index != -1) {
+            workbook.removeSheetAt(index);
+        }
+        return workbook.createSheet(sheetName);
+    }
+
+    /*private void writeDeliveryNoteExcel(XSSFWorkbook workbook, LocalDate startDate, LocalDate endDate) {
         List<OrderComposition> orderCompositions = orderCompositionQueryService.findOrderCompositionAndDateBetween(startDate, endDate);
 
         int index = workbook.getSheetIndex("Delivery Note");
@@ -101,7 +109,7 @@ public class OrderCompositionExcelServiceImpl implements OrderCompositionExcelSe
         Row footerRow = sheet.createRow(rowCount);
 
         createOrderCell(footerRow, ZERO_COLUMN_INDEX, "Составил" + "_".repeat(20), authorStyle);
-    }
+    }*/
 
     private void createOrderCell(Row row, int columnCount, Object valueOfCell, CellStyle style) {
         sheet.autoSizeColumn(columnCount);
@@ -141,6 +149,96 @@ public class OrderCompositionExcelServiceImpl implements OrderCompositionExcelSe
         ));
 
         return style;
+    }
+
+    private void writeDeliveryNoteExcel(XSSFWorkbook workbook, LocalDate startDate, LocalDate endDate) {
+        List<OrderComposition> orderCompositions = getOrderCompositions(startDate, endDate);
+
+        createDeliveryNoteSheet(workbook, startDate, endDate);
+
+        int rowCount = 4;
+        int rowId = 1;
+
+        CellStyle tableStyle = stylizeExcelService.stylizeWorkbook(workbook);
+        XSSFFont textFont = workbook.createFont();
+        textFont.setFontHeight(12);
+        tableStyle.setFont(textFont);
+
+        for (OrderComposition orderComposition : orderCompositions) {
+            createOrderRow(orderComposition, rowCount++, rowId++, tableStyle);
+        }
+
+        createAuthorFooter(workbook, rowCount, textFont);
+    }
+
+    private List<OrderComposition> getOrderCompositions(LocalDate startDate, LocalDate endDate) {
+        return orderCompositionQueryService.findOrderCompositionAndDateBetween(startDate, endDate);
+    }
+
+    private void createDeliveryNoteSheet(XSSFWorkbook workbook, LocalDate startDate, LocalDate endDate) {
+        int index = workbook.getSheetIndex("Delivery Note");
+        if (index != -1) {
+            workbook.removeSheetAt(index);
+        }
+        sheet = workbook.createSheet("Delivery Note");
+
+        CellStyle dateStyle = stylizeLabel(workbook, (byte) 2, (byte) 2, (byte) 0, (byte) 4, false);
+        CellStyle titleStyle = stylizeLabel(workbook, (byte) 0, (byte) 0, (byte) 0, (byte) 4, true);
+
+        CellStyle tableHeaderStyle = stylizeExcelService.stylizeWorkbook(workbook);
+
+        XSSFFont titleFont = workbook.createFont();
+
+        XSSFFont tableHeaderFont = workbook.createFont();
+
+        titleFont.setBold(true);
+        titleFont.setFontHeight(13);
+
+        tableHeaderFont.setBold(true);
+        tableHeaderFont.setFontHeight(13);
+
+        titleStyle.setFont(titleFont);
+        tableHeaderStyle.setFont(tableHeaderFont);
+
+        createTitleRow(startDate, endDate, dateStyle);
+        createHeaderRow(titleStyle);
+        createTableHeaderRow(tableHeaderStyle);
+    }
+
+    private void createTitleRow(LocalDate startDate, LocalDate endDate, CellStyle dateStyle) {
+        Row dateRow = sheet.createRow(2);
+        createOrderCell(dateRow, 0, "от " + startDate + " до " + endDate, dateStyle);
+    }
+
+    private void createHeaderRow(CellStyle titleStyle) {
+        Row headerRow = sheet.createRow(0);
+        createOrderCell(headerRow, 0, "ТОВАРНАЯ НАКЛАДНАЯ", titleStyle);
+    }
+
+    private void createTableHeaderRow(CellStyle tableHeaderStyle) {
+        Row tableHeaderRow = sheet.createRow(3);
+        createOrderCell(tableHeaderRow, 0, "№", tableHeaderStyle);
+        createOrderCell(tableHeaderRow, 1, "Наименование", tableHeaderStyle);
+        createOrderCell(tableHeaderRow, 2, "Количество", tableHeaderStyle);
+        createOrderCell(tableHeaderRow, 3, "Цена", tableHeaderStyle);
+        createOrderCell(tableHeaderRow, 4, "Сумма", tableHeaderStyle);
+    }
+
+    private void createOrderRow(OrderComposition orderComposition, int rowCount, int rowId, CellStyle tableStyle) {
+        Row row = sheet.createRow(rowCount);
+        createOrderCell(row, 0, rowId, tableStyle);
+        createOrderCell(row, 1, orderComposition.getProduct().getTitle(), tableStyle);
+        createOrderCell(row, 2, orderComposition.getQuantity(), tableStyle);
+        createOrderCell(row, 3, orderComposition.getProduct().getCost(), tableStyle);
+        createOrderCell(row, 4, (orderComposition.getProduct().getCost() * orderComposition.getQuantity()), tableStyle);
+    }
+
+    private void createAuthorFooter(XSSFWorkbook workbook, int rowCount, XSSFFont textFont) {
+        CellStyle authorStyle = stylizeLabel(workbook, (byte) rowCount, (byte) rowCount, (byte) 0, (byte) 4, false);
+        authorStyle.setFont(textFont);
+
+        Row footerRow = sheet.createRow(rowCount + 1);
+        createOrderCell(footerRow, ZERO_COLUMN_INDEX, "Составил" + "_".repeat(20), authorStyle);
     }
 
     @Override
